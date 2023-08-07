cb21feb36533034b4957a2abcc88fe6200095ff6
MAADHITHYAN <141621719+MAADHITHYAN@users.noreply.github.com>
2023-08-07 17:27:39 +0530

    Add files via upload

---
+++ Immutable.py
@@ -0,0 +1,4 @@
+str="Hi"
+new_str = "Bi"
+print("old string= ",str)
+print("new string= ",new_str)
---
+++ Slice.py
@@ -0,0 +1,17 @@
+str = "PYTHON"
+
+print("str[1:5] = ", str[1:5])
+
+print("str[:6] = ", str[:6])
+
+#characters starting at index 1 and
+
+# defaults to the start of the stri
+
+print("str[1:] = ", str[1:]) # defaults to the end of the stri
+
+print("str[:] = ", str[:])
+
+# defaults to the entire string
+
+print("str[1:20] = ", str[1:20]) # an index that is too big is trui
